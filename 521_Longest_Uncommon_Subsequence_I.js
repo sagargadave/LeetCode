176134ddce0ff505d1d19b1a3605ccb30027cd8c
Sagar Gadave <sagargadave1234@gmail.com>
2025-01-27 13:40:19 +0530

    add 521_Longest_Uncommon_Subsequence_I

---
+++ 521_Longest_Uncommon_Subsequence_I.js
@@ -0,0 +1,16 @@
+/**
+ * @param {string} a
+ * @param {string} b
+ * @return {number}
+ */
+var findLUSlength = function(a, b) 
+{
+    if(a===b)    
+    {
+        return -1
+    }
+    else
+    {
+        return Math.max(a.length,b.length);
+    }
+};
