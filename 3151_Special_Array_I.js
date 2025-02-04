db89bebfeb297ceb4f92e2f48a8a5955f6a39164
Sagar Gadave <sagargadave1234@gmail.com>
2025-02-04 23:14:36 +0530

    add 3151_Special_Array_I

---
+++ 3151_Special_Array_I.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var isArraySpecial = function(nums) 
+{
+    for(let i=0; i<nums.length-1; i++)    
+    {
+        if( (nums[i] % 2 == 0 && nums[i+1] % 2 == 0 ) || (nums[i] % 2 != 0 && nums[i+1] % 2 != 0 ))
+        {
+            return false;
+        }
+    }
+
+    return true;
+};
