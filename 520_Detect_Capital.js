78c27d44fb4972795c355893d9d1c4db83324166
Sagar Gadave <sagargadave1234@gmail.com>
2025-02-08 18:12:29 +0530

    add 520_Detect_Capital

---
+++ 520_Detect_Capital.js
@@ -0,0 +1,24 @@
+/**
+ * @param {string} word
+ * @return {boolean}
+ */
+var detectCapitalUse = function(word) 
+{
+    if(word.toUpperCase() === word)
+    {
+        return true;
+    }    
+    else if(word.toLowerCase() === word)
+    {
+        return true;
+    }
+    else if ((word[0] + word.slice(1).toLowerCase()) == word) 
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+
+};
