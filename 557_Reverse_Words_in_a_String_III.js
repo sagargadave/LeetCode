c5c637da4af9c2864a9fc7b4475c337b61a045b4
Sagar Gadave <sagargadave1234@gmail.com>
2025-02-12 23:25:42 +0530

    add 557_Reverse_Words_in_a_String_III

--- 557_Reverse_Words_in_a_String_III.js
+++ 557_Reverse_Words_in_a_String_III.js
@@ -2,6 +2,9 @@
  * @param {string} s
  * @return {string}
  */
+
+// FIRST APPROACH
+
 var reverseWords = function(s) 
 {
     let i = 0, j = s.length-1,result = "",temp="";
@@ -29,5 +32,35 @@ var reverseWords = function(s)
         }
     }
 
+    return result.split('').join('');
+};
+
+// SECOND APPROACH
+
+var reverseWords = function(s) 
+{
+    let i = 0,result = "",temp="";
+
+    result = result.split('');
+
+    for(i=0; i<=s.length; i++)
+    {
+
+        if(s[i] == ' ' || i== s.length)
+        {
+            result += temp;
+            temp="";
+            
+            if(i != s.length)
+            {
+                result += s[i];
+            }
+        }            
+        else if (i != s.length)
+        {
+            temp = s[i] + temp;
+        }
+    }    
+    
     return result.split('').join('');
 };
