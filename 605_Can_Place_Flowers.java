a8894d12cee29fd9e767857ef45c8f028be58ee1
Sagar Gadave <sagargadave1234@gmail.com>
2025-03-08 10:24:08 +0530

    add 605_Can_Place_Flowers

---
+++ 605_Can_Place_Flowers.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean canPlaceFlowers(int[] flowerbed, int n) 
+    {
+
+        for(int i=0; i<flowerbed.length; i++)
+        {
+            boolean l = i == 0 || flowerbed[i - 1] == 0;
+            boolean r = i == flowerbed.length - 1 || flowerbed[i + 1] == 0;
+
+            if( r && l && flowerbed[i] == 0)
+            {
+                flowerbed[i] = 1;
+                n--;
+            }
+        }    
+
+        return n<= 0;
+    }
+}
