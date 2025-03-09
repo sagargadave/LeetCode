4114da498262bcf97abf245196450e3d1ff24372
Sagar Gadave <sagargadave1234@gmail.com>
2025-03-09 23:59:58 +0530

    add 3407_Substring_Matching_Pattern

--- 3407_Substring_Matching_Pattern.java
+++ 3407_Substring_Matching_Pattern.java
@@ -1,40 +1,11 @@
 class Solution {
-    public boolean hasMatch(String s, String p) 
-    {
-        if(p.length() == 1)
-        {
+    public boolean hasMatch(String s, String p) {
+        int index = p.indexOf('*');
+        int firstpos = s.indexOf(p.substring(0, index)); 
+        int secondpos = s.indexOf(p.substring(index + 1), firstpos + index); 
+        if (firstpos != -1 && secondpos != -1) {
             return true;
         }
-        
-        if(p.charAt(0) == '*')
-        {
-            return false;
-        }
-
-        p = p.replace("*","");
-        
-        System.out.println(p);
-
-        int count = p.length(),j = 0;
-
-        if(count == 0)
-        {
-            return true;
-        }
-
-        for(int i = 0; i<s.length(); i++)    
-        {
-            if(s.charAt(i) == p.charAt(j))
-            {
-                j++;
-
-                if(j==count)
-                {
-                    return true;
-                }
-            }
-        }
-
         return false;
     }
 }
