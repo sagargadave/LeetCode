f5d31b645432862c05966111d74d28f9fa86e2bc
Sagar Gadave <sagargadave1234@gmail.com>
2025-10-27 20:15:32 +0530

    add  274_H-Index.java

---
+++ 274_H-Index.java
@@ -0,0 +1,40 @@
+/*274. H-Index
+
+Given an array of integers citations where citations[i] is the number of citations a researcher received for their ith paper, return the researcher's h-index.
+According to the definition of h-index on Wikipedia: The h-index is defined as the maximum value of h such that the given researcher has published at least h papers that have each been cited at least h times.
+
+Example 1:
+Input: citations = [3,0,6,1,5]
+Output: 3
+Explanation: [3,0,6,1,5] means the researcher has 5 papers in total and each of them had received 3, 0, 6, 1, 5 citations respectively.
+Since the researcher has 3 papers with at least 3 citations each and the remaining two with no more than 3 citations each, their h-index is 3.
+
+Example 2:
+Input: citations = [1,3,1]
+Output: 1
+
+Constraints:
+n == citations.length
+1 <= n <= 5000
+0 <= citations[i] <= 1000
+
+Solution : 
+*/
+
+class Solution {
+    public int hIndex(int[] citations) {
+        Arrays.sort(citations); // Sort ascending
+        int n = citations.length;
+        int h = 0;
+        
+        for (int i = 0; i < n; i++) {
+            int papersWithAtLeast = n - i;  // number of papers that have at least citations[i] citations
+            if (citations[i] >= papersWithAtLeast) {
+                h = papersWithAtLeast;
+                break;
+            }
+        }
+        //0 1 3 5 6
+        return h;    
+    }
+}
