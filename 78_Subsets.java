a183c51dc349488b57aed80381047bd8e78dd17d
Sagar Gadave <sagargadave1234@gmail.com>
2025-11-21 23:54:07 +0530

    Solved 78_Subsets.java

---
+++ 78_Subsets.java
@@ -0,0 +1,41 @@
+/*
+78. Subsets
+
+Given an integer array nums of unique elements, return all possible subsets (the power set).
+
+The solution set must not contain duplicate subsets. Return the solution in any order.
+
+Example 1:
+Input: nums = [1,2,3]
+Output: [[],[1],[2],[1,2],[3],[1,3],[2,3],[1,2,3]]
+
+Example 2:
+Input: nums = [0]
+Output: [[],[0]]
+ 
+
+Constraints:
+1 <= nums.length <= 10
+-10 <= nums[i] <= 10
+All the numbers of nums are unique.
+
+Solutoion :
+*/
+
+class Solution {
+    public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        backtrack(0, nums, new ArrayList<>(), result);
+        return result;
+    }
+
+    private void backtrack(int index, int[] nums, List<Integer> current, List<List<Integer>> result) {
+        result.add(new ArrayList<>(current));  // Add current subset
+
+        for (int i = index; i < nums.length; i++) {
+            current.add(nums[i]);              // Include nums[i]
+            backtrack(i + 1, nums, current, result); // Recurse
+            current.remove(current.size() - 1); // Backtrack
+        }
+    }
+}
