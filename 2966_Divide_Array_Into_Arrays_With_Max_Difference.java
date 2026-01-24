eab2810115ebc3f0303ae0694fa1c18aa90535c2
Sagar Gadave <sagargadave1234@gmail.com>
2026-01-24 22:26:51 +0530

    Solved 2966_Divide_Array_Into_Arrays_With_Max_Difference

---
+++ 2966_Divide_Array_Into_Arrays_With_Max_Difference.java
@@ -0,0 +1,63 @@
+/*
+2966. Divide Array Into Arrays With Max Difference
+
+You are given an integer array nums of size n where n is a multiple of 3 and a positive integer k.
+Divide the array nums into n / 3 arrays of size 3 satisfying the following condition:
+The difference between any two elements in one array is less than or equal to k.
+Return a 2D array containing the arrays. If it is impossible to satisfy the conditions, return an empty array. And if there are multiple answers, return any of them.
+
+Example 1:
+Input: nums = [1,3,4,8,7,9,3,5,1], k = 2
+Output: [[1,1,3],[3,4,5],[7,8,9]]
+Explanation:
+The difference between any two elements in each array is less than or equal to 2.
+
+Example 2:
+Input: nums = [2,4,2,2,5,2], k = 2
+Output: []
+Explanation:
+Different ways to divide nums into 2 arrays of size 3 are:
+[[2,2,2],[2,4,5]] (and its permutations)
+[[2,2,4],[2,2,5]] (and its permutations)
+Because there are four 2s there will be an array with the elements 2 and 5 no matter how we divide it. since 5 - 2 = 3 > k, the condition is not satisfied and so there is no valid division.
+
+Example 3:
+Input: nums = [4,2,9,8,2,12,7,12,10,5,8,5,5,7,9,2,5,11], k = 14
+Output: [[2,2,2],[4,5,5],[5,5,7],[7,8,8],[9,9,10],[11,12,12]]
+Explanation:
+The difference between any two elements in each array is less than or equal to 14.
+
+Constraints:
+n == nums.length
+1 <= n <= 105
+n is a multiple of 3
+1 <= nums[i] <= 105
+1 <= k <= 105
+
+Solution :
+*/
+
+class Solution {
+    public int[][] divideArray(int[] nums, int k) {
+
+        int n = nums.length, l = 0;
+        int result[][] = new int[n / 3][3];
+
+        Arrays.sort(nums);
+
+        for (int i = 0; i < n; i += 3) {
+            
+            int ind = 0;
+            if (nums[i+2] - nums[i] <= k) {
+                result[l][ind++] = nums[i];
+                result[l][ind++] = nums[i+1];
+                result[l][ind++] = nums[i+2];
+            } else {
+                return new int[0][0];
+            }
+            l++;
+
+        }
+        return result;
+    }
+}
