833dadb2cf2afd6e8f55168256e14a384cb44e42
Sagar Gadave <sagargadave1234@gmail.com>
2026-03-19 21:50:53 +0530

    Solved 704_Binary_Search

--- 704_Binary_Search.java
+++ 704_Binary_Search.java
@@ -36,3 +36,26 @@ class Solution {
         return -1;
     }
 }
+
+// Optimal
+class Solution {
+    public int search(int[] nums, int target) {
+
+        int left = 0, right = nums.length-1;
+
+        while (left <= right) {
+
+            int mid = (right + left) / 2;
+
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else if (nums[mid] > target) {
+                right = mid - 1;
+            }
+        }
+
+        return -1;
+    }
+}
