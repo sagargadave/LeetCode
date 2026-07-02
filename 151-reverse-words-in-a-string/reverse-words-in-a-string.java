ad593a4349507c3752c05fcdea3b85d00b404ed8
Sagar Gadave <sagargadave1234@gmail.com>
2026-07-02 11:17:56 +0530

    Time: 23 ms (5.40%) | Memory: 52.5 MB (5.22%) - LeetSync

--- 151-reverse-words-in-a-string/reverse-words-in-a-string.java
+++ 151-reverse-words-in-a-string/reverse-words-in-a-string.java
@@ -21,26 +21,7 @@ class Solution {
         }
 
         result = result + " " + s1;
-        n = result.length();
 
-        if (result.charAt(n - 1) == ' ') {
-            for (int i = n - 1; i >= 0; i--) {
-                if (result.charAt(i) == ' ') {
-                    result = result.substring(0, i);
-                    break;
-                }
-            }
-        }
-
-        if (result.charAt(0) == ' ') {
-            for (int i = 0; i < n; i++) {
-                if (result.charAt(i) != ' ') {
-                    result = result.substring(i, n);
-                    break;
-                }
-            }
-        }
-
-        return result;
+        return result.trim();
     }
 }
