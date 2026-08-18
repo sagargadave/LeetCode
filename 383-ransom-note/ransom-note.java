3ce0c01887119bc1e15a2edf090ea8b514279485
Sagar Gadave <sagargadave1234@gmail.com>
2026-08-18 15:43:18 +0530

    Time: 1 ms (99.96%) | Memory: 46.3 MB (68.34%) - LeetSync

--- 383-ransom-note/ransom-note.java
+++ 383-ransom-note/ransom-note.java
@@ -1,18 +1,18 @@
 class Solution {
     public boolean canConstruct(String ransomNote, String magazine) {
 
-        int arr[] = new int[26];
+        int count[] = new int[26];
 
         for (char ch : ransomNote.toCharArray()) {
-            arr[ch - 'a']++;
+            count[ch - 'a']++;
         }
 
         for (char ch : magazine.toCharArray()) {
-            arr[ch - 'a']--;
+            count[ch - 'a']--;
         }
 
         for (int i = 0; i < 26; i++) {
-            if (arr[i] > 0) {
+            if (count[i] > 0) {
                 return false;
             }
         }
