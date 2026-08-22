eb6e688801cdc680e98501d5acf85b47f250dac4
Sagar Gadave <sagargadave1234@gmail.com>
2026-08-22 21:43:12 +0530

    Time: 1 ms (99.93%) | Memory: 43.1 MB (57.92%) - LeetSync

--- 409-longest-palindrome/longest-palindrome.java
+++ 409-longest-palindrome/longest-palindrome.java
@@ -1,40 +1,24 @@
 class Solution {
     public int longestPalindrome(String s) {
 
-        int cFrq[] = new int[26];
-        int sFrq[] = new int[26];
-
-        int result = 0, odd = 0;
+        int freq[] = new int[128];
+        int result = 0;
 
         for (char ch : s.toCharArray()) {
-            if (ch >= 65 && ch <= 91) {
-                cFrq[ch - 'A']++;
-            } else {
-                sFrq[ch - 'a']++;
+            freq[ch]++;
+        }
+
+        boolean odd = false;
+
+        for (int count : freq) {
+            result += (count / 2) * 2;
+
+            if (count % 2 != 0) {
+                odd = true;
             }
         }
 
-        boolean flag = true;
-
-        for (int i = 0; i < 26; i++) {
-
-            if ((cFrq[i] % 2 != 0)) {
-                flag = false;
-                result += cFrq[i] - 1;
-            }
-            if ((sFrq[i] % 2 != 0)) {
-                flag = false;
-                result += sFrq[i] - 1;
-            }
-            if (sFrq[i] != 0 && sFrq[i] % 2 == 0) {
-                result += sFrq[i];
-            }
-            if (cFrq[i] != 0 && cFrq[i] % 2 == 0) {
-                result += cFrq[i];
-            }
-        }
-
-        if (!flag) {
+        if (odd) {
             result++;
         }
 
