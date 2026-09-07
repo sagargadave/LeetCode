60d6821bd9d054da6fbfa801a6bae2e2463e9c1a
Sagar Gadave <sagargadave1234@gmail.com>
2026-09-07 07:16:26 +0530

    Time: 0 ms (100.00%) | Memory: 45.6 MB (86.61%) - LeetSync

--- 861-flipping-an-image/flipping-an-image.java
+++ 861-flipping-an-image/flipping-an-image.java
@@ -3,15 +3,13 @@ class Solution {
 
         int col = image.length, row = image[0].length;
 
-        int start = 0;
-
         for (int i = 0; i < col; i++) {
-            int end = row - 1;
-            start = 0;
+            int start = 0, end = row - 1;
+
             while (start <= end) {
                 int temp = 1 - image[i][start];
-                image[i][start] = 1 - image[i][(row - 1) - start];
-                image[i][(row - 1) - start] = temp;
+                image[i][start] = 1 - image[i][end];
+                image[i][end] = temp;
                 start++;
                 end--;
             }
