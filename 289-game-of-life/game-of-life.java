dd1fa5ff7136edcd8318b1c8170d76884133df5c
Sagar Gadave <sagargadave1234@gmail.com>
2026-09-20 10:50:18 +0530

    Time: 0 ms (100.00%) | Memory: 43.1 MB (64.04%) - LeetSync

--- 289-game-of-life/game-of-life.java
+++ 289-game-of-life/game-of-life.java
@@ -1,35 +1,68 @@
 class Solution {
     public void gameOfLife(int[][] board) {
-        int rows = board.length, cols = board[0].length;
-        int[][] tempBoard = new int[rows][cols];
-        
-        for (int r = 0; r < rows; r++) {
-            for (int c = 0; c < cols; c++) {
+
+        int rows = board.length;
+        int cols = board[0].length;
+
+
+        int[][] directions = {
+            {-1, -1}, {-1, 0}, {-1, 1},
+            {0, -1},           {0, 1},
+            {1, -1},  {1, 0},  {1, 1}
+        };
+
+
+        for (int i = 0; i < rows; i++) {
+
+            for (int j = 0; j < cols; j++) {
+
                 int liveNeighbors = 0;
-                for (int i = -1; i <= 1; i++) {
-                    for (int j = -1; j <= 1; j++) {
-                        if (i == 0 && j == 0) continue; // skip itself
-                        liveNeighbors += neighbor(board, r + i, c + j);
+
+
+                for (int[] dir : directions) {
+
+                    int ni = i + dir[0]; 
+                    int nj = j + dir[1]; 
+
+                    if (ni >= 0 && ni < rows &&
+                        nj >= 0 && nj < cols) {
+
+
+                        if (board[ni][nj] == 1 ||
+                            board[ni][nj] == 2) {
+
+                            liveNeighbors++;
+                        }
                     }
                 }
-                
-                if (board[r][c] == 1) {
-                    tempBoard[r][c] = (liveNeighbors < 2 || liveNeighbors > 3) ? 0 : 1;
-                } else {
-                    tempBoard[r][c] = (liveNeighbors == 3) ? 1 : 0;
+
+                if (board[i][j] == 1) {
+
+                    if (liveNeighbors < 2 || liveNeighbors > 3) {
+                        board[i][j] = 2;  // 1 -> 0
+                    }
+
+                }
+                else if (board[i][j] == 0) {
+
+                    if (liveNeighbors == 3) {
+                        board[i][j] = 3;  // 0 -> 1
+                    }
                 }
             }
         }
-        
-        for (int r = 0; r < rows; r++) {
-            for (int c = 0; c < cols; c++) {
-                board[r][c] = tempBoard[r][c];
+
+        for (int i = 0; i < rows; i++) {
+
+            for (int j = 0; j < cols; j++) {
+
+                if (board[i][j] == 2) {
+                    board[i][j] = 0;
+                }
+                else if (board[i][j] == 3) {
+                    board[i][j] = 1;
+                }
             }
         }
-    }
-
-    private int neighbor(int[][] board, int r, int c) {
-        if (r < 0 || r >= board.length || c < 0 || c >= board[0].length) return 0;
-        return board[r][c];
     }
 }
